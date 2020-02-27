b503558c78d5f1af2e371aab6de6598ed7a155e4
雪糕 <blood@bellcode.com>
2020-02-27 11:53:33 +0800

    native加入logo图片

--- platform/pc/src/renderer/renderer.js
+++ platform/pc/src/renderer/renderer.js
@@ -3,15 +3,13 @@
  * @desc 渲染进程逻辑类
  * @date 2020-02-18 11:44:51 
  * @Last Modified by: 雪糕
- * @Last Modified time: 2020-02-26 21:59:54
+ * @Last Modified time: 2020-02-27 11:17:02
  */
 import * as logger from './logger.js';
 import * as message from './message.js';
 
 /** 初始化 */
-export function init() {
-    logger.log('renderer', `初始化渲染进程主逻辑`);
+logger.log('renderer', `初始化渲染进程主逻辑`);
 
-    //初始化消息处理类
-    message.init();
-}
+//初始化消息处理类
+message.init();
