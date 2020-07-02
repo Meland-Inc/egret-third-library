9201f90847f1df74b9e1e6c2eef99522fe59870e
xiangqian <807929448@qq.com>
2020-07-03 02:26:54 +0800

    修改引擎发布，让游戏代码加上闭包并解决反射问题和入口问题

--- egretEngine/5.2.9/tools/tasks/compile.js
+++ egretEngine/5.2.9/tools/tasks/compile.js
@@ -7,8 +7,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    var _ = { label: 0, sent: function () { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function () { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
@@ -127,6 +127,8 @@ var UglifyPlugin = /** @class */ (function () {
                         }
                         return code;
                     }));
+                    //加上闭包环境并将类加入命名空间用于反射 utils.js.closure()
+                    jscode = utils.closure(jscode, 'bellPlanet');
                     pluginContext.createFile(matcher.target, new Buffer(jscode));
                 }
                 return [2 /*return*/];
