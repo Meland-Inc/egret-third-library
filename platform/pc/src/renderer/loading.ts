6bd3ebb21ac2035aa3baa7d973d6e6cd12ee5324
雪糕 <blood@bellcode.com>
2020-07-02 23:50:34 +0800

    fix: 修复native更新时,进度条报错,导致更新不了的问题

--- platform/pc/src/renderer/loading.ts
+++ platform/pc/src/renderer/loading.ts
@@ -4,28 +4,42 @@
  * @Date 2020-02-13 14:55:57
  * @FilePath \pc\src\renderer\loading.ts
  */
-let loadingGroup = document.getElementById('loadingGroup') as HTMLDivElement;
-let loadingTip = document.getElementById('loadingTip') as HTMLDivElement;
-let loadingProgress = document.getElementById('loadingProgress') as HTMLProgressElement;
+function getLoadingGroup() {
+    return document.getElementById('loadingGroup') as HTMLDivElement;
+}
+
+function getLoadingTip() {
+    return document.getElementById('loadingTip') as HTMLDivElement;
+}
+
+function getLoadingProgress() {
+    return document.getElementById('loadingProgress') as HTMLProgressElement;
+}
 
 let timeoutId: number;
 export function showLoading(value: string) {
     clearTimeout();
+    const loadingGroup = getLoadingGroup();
+    const loadingTip = getLoadingTip();
     loadingGroup.hidden = false;
     loadingTip.textContent = value;
 }
 
 export function hideLoading() {
     clearTimeout();
+    const loadingGroup = getLoadingGroup();
+    const loadingTip = getLoadingTip();
     loadingGroup.hidden = true;
     loadingTip.textContent = "";
 }
 
 export function setLoadingProgress(value: number) {
+    const loadingProgress = getLoadingProgress();
     loadingProgress.value = value;
 }
 
 export function gradualProgress() {
+    const loadingProgress = getLoadingProgress();
     loadingProgress.value += 1;
     clearTimeout();
     if (loadingProgress.value === 100) {
