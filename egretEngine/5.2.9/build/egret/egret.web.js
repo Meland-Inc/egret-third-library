b732d20d863b2bd2b2aeba69e426207b0b48895c
Felix <17750020060@163.com>
2020-10-09 17:23:32 +0800

    修复白鹭渲染很多文本内存爆了问题

--- egretEngine/5.2.9/build/egret/egret.web.js
+++ egretEngine/5.2.9/build/egret/egret.web.js
@@ -7925,9 +7925,10 @@ var egret;
                 if (!this.canvasRenderBuffer.context) {
                     return;
                 }
-                if (canvasScaleX != 1 || canvasScaleY != 1) {
-                    this.canvasRenderBuffer.context.setTransform(canvasScaleX, 0, 0, canvasScaleY, 0, 0);
-                }
+                //dirtyRender为false也会执行,导致显示很多文本时内存一直增长
+                // if (canvasScaleX != 1 || canvasScaleY != 1) {
+                //     this.canvasRenderBuffer.context.setTransform(canvasScaleX, 0, 0, canvasScaleY, 0, 0);
+                // }
                 if (x || y) {
                     if (node.dirtyRender) {
                         this.canvasRenderBuffer.context.setTransform(canvasScaleX, 0, 0, canvasScaleY, -x, -y);
